d0fde760d3b518be0cac007e5fe8a514720f1b0b
RealJe-emy <126858904+RealJe-emy@users.noreply.github.com>
2024-11-15 21:30:57 +0300

    Updates

--- converter.py
+++ converter.py
@@ -11,15 +11,29 @@ def main():
     else:
         path = os.getcwd() + '/'
 
-    name = pytube.extract.video_id(video_url)
-    YouTube(video_url).streams.filter(only_audio=True).first().download(filename=name)
-    location = path + name + '.mp4'
-    renametomp3 = path + name + '.mp3'
+    try:
+        name = pytube.extract.video_id(video_url)
+        audio_stream = YouTube(video_url).streams.filter(only_audio=True).first()
 
-    if os.name == 'nt':
-        os.system('ren {0} {1}'.format(location, renametomp3))
-    else:
-        os.system('mv {0} {1}'.format(location, renametomp3))
+        if not audio_stream:
+            print("No audio stream available for this video.")
+            return
+
+        audio_stream.download(filename=name)
+        location = path + name + '.mp4'
+        renametomp3 = path + name + '.mp3'
+
+        if os.name == 'nt':
+            os.system(f'ren {location} {renametomp3}')
+        else:
+            os.system(f'mv {location} {renametomp3}')
+
+        print(f"Audio downloaded and saved as {renametomp3}")
+
+    except pytube.exceptions.VideoUnavailable:
+        print("The video is unavailable. Please check the URL.")
+    except Exception as e:
+        print("An error occurred:", e)
 
 
 if __name__ == '__main__':
